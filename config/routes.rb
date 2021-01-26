bf556bea4043741022820dc7f9e722ad2a96ca60
feebas7312 <trp.u-mis0609ssno.asce-0101@ezweb.ne.jp>
2021-01-26 23:33:54 +0900

    adminログインとログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'admins#index'
+
   devise_for :admins, controllers: {
     sessions: 'admins/sessions',
     passwords: 'admins/passwords',
     registrations: 'admins/registrations'
   }
+
+  resources :admins, only: :index
 end
