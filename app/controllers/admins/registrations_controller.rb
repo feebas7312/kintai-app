e16d495c30579c5cfe4339351e2621f79ee004ea
feebas7312 <trp.u-mis0609ssno.asce-0101@ezweb.ne.jp>
2021-01-30 04:22:12 +0900

    admin編集機能の実装

--- app/controllers/admins/registrations_controller.rb
+++ app/controllers/admins/registrations_controller.rb
@@ -2,7 +2,7 @@
 
 class Admins::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -65,9 +65,9 @@ class Admins::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:number, :last_name, :first_name, :email])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
