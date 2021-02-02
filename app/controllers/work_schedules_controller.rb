8670a65a3f8763cb2bd1e335aa8698f2b5c9d355
feebas7312 <trp.u-mis0609ssno.asce-0101@ezweb.ne.jp>
2021-02-03 07:47:57 +0900

    ページビュー修正

--- app/controllers/work_schedules_controller.rb
+++ app/controllers/work_schedules_controller.rb
@@ -1,10 +1,26 @@
 class WorkSchedulesController < ApplicationController
   def new
+    # @admin = Admin.find(current_admin.id)
+    # @employees = Employee.where(admin_id: @admin.id)
+    # @year = Date.today.year
+    # @month = Date.today.month
+    # @days = Date.today.end_of_month.day
     @admin = Admin.find(current_admin.id)
     @employees = Employee.where(admin_id: @admin.id)
-    @year = Date.today.year
-    @month = Date.today.month
-    @days = Date.today.end_of_month.day
+    if params["search_date(1i)"].nil? || params["search_date(2i)"].nil?
+      @year = Date.today.year
+      @month = Date.today.month
+      @days = Date.today.end_of_month.day
+      @works = SearchWorkSchedulesService.search(@year, @month)
+    elsif params["search_date(1i)"].empty? || params["search_date(2i)"].empty?
+      flash[:alert] = '⚠️⚠️⚠️ 年と月を入力してください ⚠️⚠️⚠️'
+      redirect_back fallback_location: :search
+    else
+      @year = params["search_date(1i)"].to_i
+      @month = params["search_date(2i)"].to_i
+      @days = Date.new(@year, @month, -1).day
+      @works = SearchWorkSchedulesService.search(@year, @month)
+    end
   end
 
   def search
