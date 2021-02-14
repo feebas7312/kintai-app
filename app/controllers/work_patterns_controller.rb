0ad676d8721944dab0fb5e03f15a7f7ca1348b04
feebas7312 <trp.u-mis0609ssno.asce-0101@ezweb.ne.jp>
2021-02-14 14:35:10 +0900

    work_pattern登録機能の実装

---
+++ app/controllers/work_patterns_controller.rb
@@ -0,0 +1,21 @@
+class WorkPatternsController < ApplicationController
+  def new
+    @work_pattern = WorkPattern.new
+    @company = Company.find(params[:company_id])
+  end
+
+  def create
+    @work_pattern = WorkPattern.new(work_pattern_params)
+    if @work_pattern.save
+      redirect_to company_path(current_admin)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def work_pattern_params
+    params.require(:work_pattern).permit(:start_time, :end_time).merge(company_id: params[:company_id])
+  end
+end
