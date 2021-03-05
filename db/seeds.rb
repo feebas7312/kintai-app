4aafe474ba4d80b67d09083f14b4525ec38640f8
feebas7312 <trp.u-mis0609ssno.asce-0101@ezweb.ne.jp>
2021-03-06 05:13:38 +0900

    seed編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,10 @@ Admin.create(
 
 Company.create(
   name: 'テスト会社',
+  postal_code: '730-8511',
+  address: '広島県広島市中区基町10-52',
+  phone_number: '082-228-2111',
+  cutoff_date_id: 4,
   opening_time: '09:00:00',
   closing_time: '22:00:00',
   admin_id: 1
@@ -19,8 +23,8 @@ Company.create(
     [
       {
         number: "F35155#{i+1}",
-        last_name: "スタッフ#{i+1}",
-        first_name: "test",
+        last_name: "スタッフ",
+        first_name: "#{i+1}",
         joining_date: "2020-09-20",
         password: "password",
         password_confirmation: "password",
@@ -29,3 +33,11 @@ Company.create(
     ]
   )
 end
+
+WorkPattern.create(
+  [
+    { start_time: '0900', end_time: '1800', company_id: 1 },
+    { start_time: '1100', end_time: '2000', company_id: 1 },
+    { start_time: '1300', end_time: '2200', company_id: 1 }
+  ]
+)
