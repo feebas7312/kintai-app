b268c8bbf75b74fbcee7a4ac093b15ef9eb2f6db
feebas7312 <trp.u-mis0609ssno.asce-0101@ezweb.ne.jp>
2021-03-06 07:17:25 +0900

    編集ページ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Company.create(
   name: 'テスト会社',
   postal_code: '730-8511',
   address: '広島県広島市中区基町10-52',
-  phone_number: '082-228-2111',
+  phone_number: '0822282111',
   cutoff_date_id: 4,
   opening_time: '09:00:00',
   closing_time: '22:00:00',
