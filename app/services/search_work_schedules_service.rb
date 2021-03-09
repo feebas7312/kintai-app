83776df0fde136657ebd596a02b24c91ddd697b9
feebas7312 <trp.u-mis0609ssno.asce-0101@ezweb.ne.jp>
2021-03-09 10:03:35 +0900

    検索機能の調整

--- app/services/search_work_schedules_service.rb
+++ app/services/search_work_schedules_service.rb
@@ -1,5 +1,13 @@
 class SearchWorkSchedulesService
-  def self.search(year, month)
-    WorkSchedule.where('work_date LIKE(?)', "#{year}-%#{month}-%")
+  def self.search(year, month, days, start_day, admin, employees)
+    start_date = Date.new(year, month, start_day)
+    end_date = start_date + days - 1
+
+    employee_ids = []
+    employees.each do |employee|
+      employee_ids << employee.id
+    end
+
+    WorkSchedule.where(work_date: start_date..end_date, admin_id: admin.id).or(WorkSchedule.where(work_date: start_date..end_date, employee_id: employee_ids))
   end
 end
