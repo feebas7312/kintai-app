21d228e66ea9bb75e76d4d44d540576306c71e74
feebas7312 <trp.u-mis0609ssno.asce-0101@ezweb.ne.jp>
2021-03-09 10:02:48 +0900

    ActiveHash情報追加

--- app/models/cutoff_date.rb
+++ app/models/cutoff_date.rb
@@ -1,11 +1,11 @@
 class CutoffDate < ActiveHash::Base
   self.data = [
-    { id: 1, name: '5日' },
-    { id: 2, name: '10日' },
-    { id: 3, name: '15日' },
-    { id: 4, name: '20日' },
-    { id: 5, name: '25日' },
-    { id: 6, name: '月末' }
+    { id: 1, name: '5日', next_day: 6 },
+    { id: 2, name: '10日', next_day: 11 },
+    { id: 3, name: '15日', next_day: 16 },
+    { id: 4, name: '20日', next_day: 21 },
+    { id: 5, name: '25日', next_day: 26 },
+    { id: 6, name: '月末', next_day: 1 }
   ]
 
   include ActiveHash::Associations
