8fe060403e97e893980bca9ca21045ab61d03a89
feebas7312 <trp.u-mis0609ssno.asce-0101@ezweb.ne.jp>
2021-04-18 04:43:05 +0900

    週合計の労働時間オーバーでsubmit無効

--- app/javascript/schedule_new.js
+++ app/javascript/schedule_new.js
@@ -1,5 +1,6 @@
 function workTimeCalc(){
   const scheduleFormMonth = document.querySelectorAll(".month-input-field");
+  const submitBtn = document.getElementById("js-submit");
 
   // loadイベント（起動時の計算）
   scheduleFormMonth.forEach(function(monthForm){
@@ -77,10 +78,29 @@ function workTimeCalc(){
         });
 
         weekTotal.value = (weekTotalCalc / 60).toLocaleString(undefined, { maximumFractionDigits: 1 }) + "時間";
+        if ((scheduleFormDay.length == 7 && weekTotalCalc > 2400) || (scheduleFormDay.length < 10 && weekTotalCalc > 2880) || (scheduleFormDay.length == 10 && weekTotalCalc > 3360)) {
+          weekTotal.setAttribute("style", "color: red; font-weight: bold;");
+        } else {
+          weekTotal.removeAttribute("style", "color: red; font-weight: bold;");
+        }
         monthTotalCalc += weekTotalCalc;
       });
 
       monthTotal.value = (monthTotalCalc / 60).toLocaleString(undefined, { maximumFractionDigits: 1 }) + "時間";
+
+      let weekTimeStyleArray = [];
+      const weekTimeArray = document.querySelectorAll(".work-time__week-total");
+      weekTimeArray.forEach( function(weekTime) {
+        weekTimeStyleArray.push(weekTime.getAttribute("style") !== "color: red; font-weight: bold;")
+      });
+
+      if (weekTimeStyleArray.every( function(value) { return value })) {
+        submitBtn.disabled = false;
+        submitBtn.removeAttribute("style", "opacity: 0.5;");
+      } else {
+        submitBtn.disabled = true;
+        submitBtn.setAttribute("style", "opacity: 0.5;");
+      }
     });
   });
 
